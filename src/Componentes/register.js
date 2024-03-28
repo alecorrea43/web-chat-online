b382a74f749bf0204d700cecb87158222f02d174
Alejandro <itznecro.4376761@gmail.com>
2024-03-28 15:44:34 -0300

    aa

--- src/Componentes/register.js
+++ src/Componentes/register.js
@@ -162,7 +162,7 @@ const Register = () => {
     setUsernameError("");
     setPasswordError("");
 
-    fetch("https://chat-web-online.netlify.app/.netlify/functions/registerUser", {
+    fetch("/.netlify/functions/registerUser", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
