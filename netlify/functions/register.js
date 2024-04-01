b44cfd6d862afb8682225c4f403200b3bc4b5464
Alejandro <itznecro.4376761@gmail.com>
2024-04-01 19:56:09 -0300

    a

--- netlify/functions/register.js
+++ netlify/functions/register.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 const bcrypt = require("bcrypt");
-const User = require("/Componentes/User");
+const User = require("../src/components/User");
 
 exports.handler = async (event) => {
   try {
