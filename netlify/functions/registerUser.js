21f16c9c910acfa44a55426cc5656b742f34b3cb
Alejandro <itznecro.4376761@gmail.com>
2024-04-03 20:24:40 -0300

    a

--- netlify/functions/registerUser.js
+++ netlify/functions/registerUser.js
@@ -1,19 +1,18 @@
 const { MongoClient } = require('mongodb');
-require('dotenv').config()
 
 exports.handler = async (event, context) => {
-  if (event.httpMethod !== "POST") {
+ if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
-  }
+ }
 
-  const { nombre, email, password } = JSON.parse(event.body);
+ const { nombre, email, password } = JSON.parse(event.body);
 
-  // Acceder a la variable de entorno MONGODB_URI
-  const uri = process.env.MONGODB_URI;
+ // Acceder a la variable de entorno MONGODB_URI
+ const uri = process.env.MONGODB_URI;
 
-  // Conectar a MongoDB
-  const client = new MongoClient(uri);
-  try {
+ // Conectar a MongoDB
+ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+ try {
     await client.connect();
     const db = client.db('test');
     const collection = db.collection('users');
@@ -25,13 +24,13 @@ exports.handler = async (event, context) => {
       statusCode: 200,
       body: JSON.stringify({ message: "Usuario registrado exitosamente", userId: result.insertedId }),
     };
-  } catch (error) {
+ } catch (error) {
     console.error(error);
     return {
       statusCode: 500,
       body: JSON.stringify({ message: "Error al registrar el usuario" }),
     };
-  } finally {
+ } finally {
     await client.close();
-  }
+ }
 };
