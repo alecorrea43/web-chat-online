b71ac85aca6e0f686312e1672bd1cc4db4ca058e
Alejandro <itznecro.4376761@gmail.com>
2024-04-03 20:41:35 -0300

    a

--- netlify/functions/registerUser.js
+++ netlify/functions/registerUser.js
@@ -11,11 +11,11 @@ exports.handler = async (event, context) => {
  const uri = process.env.MONGODB_URI;
 
  // Conectar a MongoDB
- const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+ const client = new MongoClient(uri);
  try {
     await client.connect();
-    const db = client.db('nombre_de_tu_base_de_datos'); // Asegúrate de reemplazar 'nombre_de_tu_base_de_datos' con el nombre real de tu base de datos
-    const collection = db.collection('usuarios');
+    const db = client.db('test'); // Asegúrate de reemplazar 'nombre_de_tu_base_de_datos' con el nombre real de tu base de datos
+    const collection = db.collection('users');
 
     // Aquí puedes insertar el usuario en la base de datos
     const result = await collection.insertOne({ nombre, email, password });
