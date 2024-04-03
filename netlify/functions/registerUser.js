2f6972455275ba81cbbdd1fefacf68bc6a76bb79
Alejandro <itznecro.4376761@gmail.com>
2024-04-03 15:45:14 -0300

    a

--- netlify/functions/registerUser.js
+++ netlify/functions/registerUser.js
@@ -1,7 +1,6 @@
-const {MongoClient}= require('mongodb')
+const { MongoClient } = require('mongodb');
 require('dotenv').config(); // Importa las variables de entorno desde .env
 const User = require('../../src/Pages/User'); // Importa el modelo de usuario
-
 
 const uri = process.env.MONGODB_URI;
 const dbName = process.env.MONGODB_DATABASE;
@@ -14,12 +13,15 @@ exports.handler = async (event, context) => {
     if (!client) {
       client = new MongoClient(uri);
       await client.connect();
+      console.log('Conexión a MongoDB establecida.');
     }
 
     const { name, email, password } = JSON.parse(event.body);
+    console.log('Datos recibidos:', { name, email, password });
 
     // Validar datos de entrada
     if (!name || !email || !password) {
+      console.log('Campos obligatorios faltantes.');
       return {
         statusCode: 400,
         body: JSON.stringify({ error: 'Faltan campos obligatorios.' }),
@@ -29,6 +31,7 @@ exports.handler = async (event, context) => {
     // Verificar si el usuario ya está registrado
     const existingUser = await client.db(dbName).collection(collectionName).findOne({ email });
     if (existingUser) {
+      console.log('El correo electrónico ya está registrado.');
       return {
         statusCode: 400,
         body: JSON.stringify({ error: 'El correo electrónico ya está registrado.' }),
@@ -37,8 +40,10 @@ exports.handler = async (event, context) => {
 
     // Crear un nuevo usuario
     const newUser = new User({ name, email, password });
+    console.log('Nuevo usuario:', newUser);
     await client.db(dbName).collection(collectionName).insertOne(newUser);
 
+    console.log('Usuario registrado exitosamente.');
     return {
       statusCode: 200,
       body: JSON.stringify({ message: 'Registro exitoso.' }),
