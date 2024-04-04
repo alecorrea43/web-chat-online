64280b5af72f20e285801a33044f3918bc4f7449
Alejandro <itznecro.4376761@gmail.com>
2024-04-04 13:52:39 -0300

    a

---
+++ netlify/functions/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import serverless from "serverless-http";
+import Register from "../../src/Pages/register"
+
+const app = express();
+app.use(cors());
+ 
+const port = process.env.PORT || 5000;
+const router = express.Router();
+
+
+router.post("/register", (req, res) => {
+res.json(Register.handleRegister(req));
+});
+app.use("/.netlify/functions/server", router);
+export const handler = serverless(app);
