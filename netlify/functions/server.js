aff3b5414d79f2557e02486237233d42c43e9ced
Alejandro <itznecro.4376761@gmail.com>
2024-04-04 17:58:16 -0300

    a

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -12,27 +12,17 @@ app.use(bodyParser.json());
 
 const client = new MongoClient(process.env.MONGODB_URI);
 
-app.post('/register', async (req, res) => {
- try {
-   console.log('Solicitud recibida:', req.body);
-    await client.connect();
-    const collection = client.db("test").collection("users");
-    const user = req.body;
-    const result = await collection.insertOne(user);
-    res.status(200).json({ message: "User registered successfully", userId: result.insertedId });
- } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Error registering user" });
- } finally {
-    await client.close();
- }
-});
-
-// Crear un servidor con aws-serverless-express
-const server = awsServerlessExpress.createServer(app);
-
-// Exportar la función handler para AWS Lambda
-exports.handler = (event, context) => {
- // Proxy el evento y el contexto a aws-serverless-express
- return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
-};
+// Aquí puedes agregar tus rutas y lógica de negocio
+// Por ejemplo, una ruta simple que devuelve un mensaje
+app.get('/hello', (req, res) => {
+   res.send('Hello World!');
+  });
+  
+  // Crear un servidor con aws-serverless-express
+  const server = awsServerlessExpress.createServer(app);
+  
+  // Exportar la función handler para AWS Lambda
+  exports.handler = (event, context) => {
+   // Proxy el evento y el contexto a aws-serverless-express
+   return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
+  };
