1ce368b5feefc5e0f02db381d6f39706e6a6b245
Alejandro <itznecro.4376761@gmail.com>
2024-04-12 03:15:21 -0300

    s

--- src/Pages/chat.js
+++ src/Pages/chat.js
@@ -69,8 +69,11 @@ const StyledInput2 = styled(Input)(({ theme }) => ({
   padding: "10px",
   borderBottom: "none", // Elimina el borde inferior
   "&:hover": {
-     borderBottom: "none", // Elimina el borde inferior en hover
-  },
+    borderBottom: "none", // Elimina el borde inferior en hover
+    '& textarea': {
+      borderBottom: "none !important", // Asegura que el borde inferior del textarea se elimine durante el hover
+    },
+ },
   "&:focus": {
      borderBottom: "none", // Elimina el borde inferior en focus
   },
